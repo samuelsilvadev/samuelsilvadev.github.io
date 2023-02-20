634e76ccc4400c85d9719dfcc6d60c69fddd8cf6
Samuel Silva <samuelsilvawb@gmail.com>
2023-02-20 10:01:50 +0000

    feat: add simple elements on home page and 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,22 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import { Link, HeadFC, PageProps } from "gatsby";
+import { colors } from "../styles/constants";
 
 const pageStyles = {
-  color: "#232129",
-  padding: "96px",
+  color: colors.primary,
+  padding: 48,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
-
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+};
+
+export const Head: HeadFC = () => <title>Samuel Silva - Not found</title>;
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
@@ -29,21 +24,10 @@ const NotFoundPage: React.FC<PageProps> = () => {
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
       </p>
+      <Link to="/">Go home</Link>.
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
+export default NotFoundPage;
---
+++ src/styles/constants.ts
@@ -0,0 +1,4 @@
+export const colors = {
+  primary: "#232129",
+  accent: "#663399",
+};
