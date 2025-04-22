5bac15250da6c4eeada17add71485262a97cb466
Samuel Silva <samuelsilvawb@gmail.com>
2025-04-22 23:36:15 +0100

    fix: types error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
+import * as React from "react";
 import "../styles/global.css";
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return children;
+  return <>{children}</>;
 }
 
 export default Layout;
