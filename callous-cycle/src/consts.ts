52c2a6a3a23d4e99db5f86863780a29fa838404d
Samuel Silva <samuelsilvawb@gmail.com>
2025-04-27 17:22:20 +0100

    chore: prepare new version

--- callous-cycle/src/consts.ts
+++ callous-cycle/src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Samuel Silva's Blog";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog where I share my thoughts and experiences about web development, design, and technology.";
+  "Welcome to my blog where I share my thoughts and experiences about web development, technology, and more.";
